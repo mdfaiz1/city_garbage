2e37384be29457e3a0b6a5d258dc8fcf7b0c1ca1
mdfaiz9 <mdfaizalam827@gmail.com>
2025-01-26 13:35:42 +0530

    change

--- src/app.js
+++ src/app.js
@@ -14,9 +14,6 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.get("/", (req, res) => {
-  res.status(200).send("hello city clean");
-});
 //routes import
 import userRouter from "./routes/user.routes.js";
 import garbageRouter from "./routes/garbage.route.js";
@@ -24,5 +21,8 @@ import garbageRouter from "./routes/garbage.route.js";
 // routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/garbage", garbageRouter);
+app.get("/", (req, res) => {
+  res.status(200).send("hello city clean");
+});
 
 export { app };
