101e60ed4bee161dc88e0cea3f5b4b1c2fc8eb33
mdfaiz9 <mdfaizalam827@gmail.com>
2025-01-26 13:03:48 +0530

    add vercel file and change in package file

--- src/app.js
+++ src/app.js
@@ -14,7 +14,9 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-
+app.get("/", (req, res) => {
+  res.status(200).send("hello city clean");
+});
 //routes import
 import userRouter from "./routes/user.routes.js";
 import garbageRouter from "./routes/garbage.route.js";
